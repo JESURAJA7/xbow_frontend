472086791a805d7296ba3b7ddcaf7d8b1fd0c325
JESURAJA7 <jesu7raja7@gmail.com>
2025-08-20 15:59:43 +0530

    fix error

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
